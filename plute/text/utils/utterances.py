8307112a167ee480d28ea285658b7dae8ce69f7b
AmreshVenugopal <amresh.venugopal@gmail.com>
2020-05-12 22:49:15 +0530

    refactor(plute/text/utils/utterances.py): renamed from plute/text/utils/alternatives.py add: function to merge utterances `merge_utterances`. add: function to convert utterances to transcripts `utterances_to_texts`. update: function that serializes utterances to a single transcript, uses the `merge_utterances` and `utterances_to_texts` functions.

--- plute/text/utils/utterances.py
+++ plute/text/utils/utterances.py
@@ -10,27 +10,80 @@ and
 ```
 as alternatives.
 """
-from typing import List, Dict
+import itertools
+from functools import reduce
+from operator import mul
+from typing import List, Optional
+from plute.types import Utterances, Transcript
 
 
-def flatten_alternatives(alternatives_list: List[List[Dict]], field="transcript", text_only=False) -> List[str]:
+def merge_utterances(utterances: Utterances) -> Utterances:
     """
-    Flatten a list of alternatives.
-    :param alternatives_list: Represents ASR transcription output.
-    :param field: The field within alternatives that contains text.
-    :return:
+    Merge utterances into a single utterance.
+
+    At times when the user is speaking with gaps, we get more than one utterance from
+    the ASR.
+
+    To make sure that the rest of the systems work fine, we merge those
+    results using a cross join and re-rank them using the following two attributes:
+    1. Sum of index for each utterance
+    2. Product of confidence values
+
+    NOTE: We are still returning items as if the ASR returned a single utterance.
+          Also the confidence values here will be very low so care must be taken
+          in interpretation.
     """
-    return [alternative[field] if text_only else alternative
-            for alternatives in alternatives_list for alternative in alternatives]
+    merged = []
+
+    def join_transcripts(transcripts):
+        return " ".join(text.strip() for text in transcripts)
+
+    indexed_utterances = [enumerate(utterance) for utterance in utterances]
+
+    for utterance_product in itertools.product(*indexed_utterances):
+        # utterance_product represents each combination of alternatives across utterances.
+        # looks like:
+        # ...
+        # ((i, k::Alternative), (i, l::Alternative))
+        # ((i, k::Alternative), (i + 1, l::Alternative)
+        # ...
+        # Where i -> index of alternative within the utterance.
+        # and k, l denote different utterances.
+        if utterance_product:
+            index_sum = sum(idx for idx, _ in utterance_product)
+            alternative = {
+                "transcript": join_transcripts([alternative["transcript"] for _, alternative in utterance_product]),
+                "confidence": reduce(mul, [alternative["confidence"] or 0 for _, alternative in utterance_product])
+            }
+            merged.append((index_sum, alternative))
+    merged = sorted(merged, key=lambda utterance: (utterance[0], -utterance[1]["confidence"] or 0))[:10]
+    return [[utterance for _, utterance in merged]]
 
 
-def serialize_alternatives(alternatives_list: List[List[Dict]], field="transcript") -> str:
+def are_utterances_null(utterances: List[Optional[List]]) -> bool:
+    return (not utterances) or (utterances == [[]])
+
+
+def utterances_to_texts(utterances: Utterances) -> List[Transcript]:
+    """
+    Return list of texts from single utterance alternatives. If there are more
+    than one utterances, we only pick the first one.
+    """
+    if are_utterances_null(utterances):
+        return []
+
+    if isinstance(utterances[0], list):
+        return [alternative["transcript"] for alternative in utterances[0]]
+
+    return []
+
+
+def serialize_utterances(utterances: Utterances) -> str:
     """
     Serialize a list of strings to string.
     Useful for feature creation.
-    :param alternatives_list: Represents ASR transcription output.
-    :param field: The field within alternatives that contains text.
-    :return:
     """
-    flat_alternatives = flatten_alternatives(alternatives_list, field=field, text_only=True)
+    flat_alternatives = utterances_to_texts(merge_utterances(utterances))
+    if not flat_alternatives:
+        return ""
     return "<s> " + " <s></s> ".join(flat_alternatives) + " </s>"
